afdb70f4780b24bbc847d7581eefa6af3cefb0d8
maxiee <maxieewong@gmail.com>
2022-11-21 13:36:28 +0800

    test init collection columns

--- test/ray_db_test.dart
+++ test/ray_db_test.dart
@@ -1,4 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:ray_db/src/collection.dart';
+import 'package:ray_db/src/column.dart';
+import 'package:ray_db/src/constants.dart';
 import 'package:ray_db/src/database.dart';
 import 'package:sqlite3/sqlite3.dart' as sq;
 
@@ -18,13 +21,15 @@ void main() {
     expect(db.hasCollection('test'), true);
   });
 
-  test('test hasCollection', () async {
-    expect(db.hasCollection('test'), false);
-    db.collection('test');
-    expect(db.hasCollection('test'), true);
-    final stmt = db.db.prepare("PRAGMA table_info(test);");
-    final ret = stmt.select();
-    print(ret);
+  test('test init collection columns', () async {
+    Collection collection = db.collection('test');
+    expect(collection.columns.containsKey('id'), true);
+    Column column = collection.columns['id']!;
+    expect(column.name, 'id');
+    expect(column.type, DataType.INTEGER);
+    expect(column.notNull, false);
+    expect(column.defaultValue, null);
+    expect(column.primaryKey, true);
   });
 
   test('test hasCollection', () async {
